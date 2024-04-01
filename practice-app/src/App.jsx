cd4f900b1db08f1fe7b63d1e09d841f5fa8e71bd
Razi <raziazam.n@gmail.com>
2024-04-01 18:01:55 +0530

    updating App.jsx

--- practice-app/src/App.jsx
+++ practice-app/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <>
-      {/* useEffect Hook afer render*/}
+      {/* useEffect Hook afer render.*/}
       <UseEffectAfterRender />
       {/* 02 useState with Arrays
       <WithArray /> */}
