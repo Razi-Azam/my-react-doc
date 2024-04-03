4c25b0998b25a9ea667cab5d5a6e27cffb76ea0f
Razi <106505820+Razi-Azam@users.noreply.github.com>
2024-04-03 23:48:47 +0530

    Adding more examples to FetchData using useEffect hook

--- practice-app/src/components/02useEffect-hook/06FecthData.jsx
+++ practice-app/src/components/02useEffect-hook/06FecthData.jsx
@@ -2,26 +2,50 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
 function  FecthData() {
-    const [posts, setPosts] = useState([])
+    const [post, setPost] = useState([])
+    const [id, setId] = useState(1)
+    const [idFromButtonClick, setIdFromButtonClick] = useState(1)
 
     useEffect(() => {
-        axios.get("https://jsonplaceholder.typicode.com/posts")
+        axios.get(`https://jsonplaceholder.typicode.com/posts/${idFromButtonClick}`)
         .then(res => {
             console.log(res)
-            setPosts(res.data)
+            setPost(res.data)
         })
         .catch(err => {
             console.log(err)
         })
-    }, [])
+    }, [idFromButtonClick])
+
+    const handleClick = () => {
+        setIdFromButtonClick(id)
+    }
 
   return (
     <div>
-        <ul className='text-xl text-lime-950 bg-slate-100 p-10'>
+        {/* Fetch data on the basis of user input and on button click
+            When the button is clicked, the handleclick event set the id from 
+            the input field to the idFromButtonClick which helps in fetching the desired data
+        */}
+        <input 
+            type="text"
+            className='border border-gray-950 bg-slate-200 text-gray-950 w-auto px-4 mx-2'
+            value={id}
+            onChange={(e) => setId(e.target.value)} 
+        />
+        <button 
+            className='border border-gray-950 bg-slate-500 text-white w-30 px-2'
+            onClick={handleClick}>Fetch Post
+        </button>
+        <h2 
+            className='text-gray-950 my-2 text-xl'>Title - {post.title}
+        </h2>
+       { /* This is a simple example of fetching data from API
+            <ul className='text-xl text-lime-950 bg-slate-100 p-10'>
             {
                 posts.map(post => <li className='mt-2' key={post.id}>{post.title}</li>)
             }
-        </ul>
+        </ul> */}
     </div>
   )
 }
