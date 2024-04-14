afb67f223d685f90f1cd5c98043aa4f0cd451475
Razi <106505820+Razi-Azam@users.noreply.github.com>
2024-04-14 23:28:38 +0530

    Adding fixes to the legacy code createStore

--- practice-app/src/react-redux/index.js
+++ practice-app/src/react-redux/index.js
@@ -20,17 +20,18 @@ const initialState = {
 /* =============================== REDUCER ==================================== */
 
 //reducer function to update the state
-//It is a pure function taht accpets state and action as arguments and returns 
+//It is a pure function that accepts state and action as arguments and returns 
 //the next state of an application
 const reducer = (state = initialState, action) => {
     switch(action.type) {
-        case BUY_CAKE: return {
-            //here, the obejct may contain more than one properties,
-            //so first make a copy of the object by using spread operator and then
-            //make change in the specifc property
-            ...state, //make a copy of the state object and then only update the number of cakes
-            numOfCakes: state.numOfCakes - 1 //not mutating the object but return a new object
-        }
+        case BUY_CAKE: 
+            return {
+                //here, the obejct may contain more than one properties,
+                //so first make a copy of the object by using spread operator and then
+                //make change in the specifc property
+                ...state, //make a copy of the state object and then only update the number of cakes
+                numOfCakes: state.numOfCakes - 1 //not mutating the object but return a new object
+            }
 
         default:
             return state
@@ -39,19 +40,32 @@ const reducer = (state = initialState, action) => {
 
 /* =============================== STORE ==================================== */
 //Responsibility 1: Holding the application state
+/* @deprecated 
+since createSTore is deprecated so it is recommended to import using alias
+import { legacy_createStore as createStore } from 'redux';
+
 const store = createStore(reducer)
+
+ */
+
+//New way of creating a store
+import { configureStore } from '@reduxjs/toolkit';
+
+const store = configureStore({
+    reducer: reducer,
+});
+
 
 //Responsibility 2: Allows acces to state via getState()
 console.log('Initial state', store.getState())
 
 //Responsibility 2: Allows the app to subscribe the changes in the store
-store.subscribe(() => console.log('Updated state ', store,getState()))
+const unsubscribe = store.subscribe(() => console.log('Updated state ', store.getState()))
 
 //Responsibility 3: Provides a dispatched method to update the state
-store.dispatch(buyCake)
-store.dispatch(buyCake)
-store.dispatch(buyCake)
+store.dispatch(buyCake())
+store.dispatch(buyCake())
+store.dispatch(buyCake())
 
 //Responsibility 4: Unsubscribe from the store
-const unsubscribe = store.subscribe(() => console.log('Updated state ', store,getState()))
 unsubscribe()
